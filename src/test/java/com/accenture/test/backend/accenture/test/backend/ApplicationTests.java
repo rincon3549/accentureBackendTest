bc3c9470fcd3c3aff558cb8faff5731482333d30
jfrinconUniajc <jfrincon@estudiante.uniajc.edu.co>
2021-03-16 19:39:55 -0500

    inicializacion proyecto

---
+++ src/test/java/com/accenture/test/backend/accenture/test/backend/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accenture.test.backend.accenture.test.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
